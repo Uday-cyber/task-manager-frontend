f48be7fc04a89a9d015bd178ad0ddbc78d844b5a
Your Name <you@example.com>
2025-11-14 18:31:32 +0530

    Refresh Token property added

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
 });
 
 API.interceptors.request.use((req) => {
@@ -13,5 +14,35 @@ API.interceptors.request.use((req) => {
 
     return req;
 });
+
+API.interceptors.response.use(
+    (req) => req,
+    async (error) => {
+        const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const { data } = await axios.post(
+          `${import.meta.env.VITE_API_URL}/users/refresh`,
+          {},
+          { withCredentials: true }
+        );
+
+        localStorage.setItem("token", data.token);
+        API.defaults.headers.common["Authorization"] = `Bearer ${data.token}`;
+
+        return API(originalRequest);
+      } catch (refreshError) {
+        console.error("Token refresh failed:", refreshError);
+        localStorage.removeItem("token");
+        window.location.href = "/";
+      }
+    }
+
+    return Promise.reject(error);
+    }
+)
 
 export default API;
